254b1aaca355654b3bb6fbfc3dccdf2f859e6870
Murad <murad.losangeles@gmail.com>
2022-08-19 19:16:26 +0300

    make gitPages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+ import React, {useEffect, useState} from "react";
 import './styles/App.css';
 import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
